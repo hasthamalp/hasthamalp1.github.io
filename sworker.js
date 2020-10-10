b96c0717e4b89b8aabf9f9488a9b985ac70b4ef8
hasthamalp <72604216+hasthamalp@users.noreply.github.com>
2020-10-10 13:12:53 +0530

    Update sworker.js

--- sworker.js
+++ sworker.js
@@ -4,7 +4,7 @@ self.addEventListener("install",e=>{
 // console.log("service worked updation")
 e.waitUntil(
     caches.open("static").then(cache=>{
-        return cache.addAll(["./","demo-one-page/agency/images/logo641.png","demo-one-page/agency/images/logo64.png"]);
+        return cache.addAll(["demo-one-page/agency/images/logo641.png","demo-one-page/agency/images/logo64.png"]);
     })
 )
 })
